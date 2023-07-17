c3c11caf818f56d38daaee7feb1415dace93d37a
salmaQueen <salmawasee9@gmail.com>
2023-07-17 15:44:22 +0300

    updated

--- shell.c
+++ shell.c
@@ -1,17 +1,9 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h> // For ssize_t
 #include <stdio.h>
 #include "shell.h"
 
 int main()
 {
 run_shell();
-  // Run command loop.
-//   lsh_loop();
-
-  // Perform any shutdown/cleanup.
 
   return EXIT_SUCCESS;
 }
